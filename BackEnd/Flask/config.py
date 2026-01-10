eb66f2f6ff440a9a8133076e4fbaa7a041fa212a
Muhammad Abu Bakar <mabubakarasif2006@gmail.com>
2026-01-10 13:53:25 +0500

    Update database configuration in config.py

--- BackEnd/Flask/config.py
+++ BackEnd/Flask/config.py
@@ -1,6 +1,7 @@
 db_config = {
     "host": "localhost",
-    "user": "root",          # MySQL username
-    "password": "AbuBaKar2006@#$",      # MySQL password
-    "database": "UTILITY_BILLING_MANAGEMENT_SYSTEM"
+    "user": "root",          # MySQL Username
+    "password": "MySQL Password",      # MySQL Password
+    "database": "UTILITY_BILLING_MANAGEMENT_SYSTEM"    #Database Name
 }
+
