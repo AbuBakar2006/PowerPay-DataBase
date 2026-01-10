afa7f4ab5af99a7ab8bd4d976218e954006ad5e6
Muhammad Abu Bakar <mabubakarasif2006@gmail.com>
2026-01-10 13:47:17 +0500

    Add files via upload

---
+++ BackEnd/Flask/config.py
@@ -0,0 +1,6 @@
+db_config = {
+    "host": "localhost",
+    "user": "root",          # MySQL username
+    "password": "AbuBaKar2006@#$",      # MySQL password
+    "database": "UTILITY_BILLING_MANAGEMENT_SYSTEM"
+}
